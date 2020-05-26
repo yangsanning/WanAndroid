515cfa2810e756b610f32c619443440647424056
杨三宁 <ysn_2020@163.com>
2020-05-26 16:34:47 +0800

    [ *] 更改为自定义 Toast

--- base-mvvm/src/main/java/ysn/com/mvvm/utils/ToastUtils.java
+++ base-mvvm/src/main/java/ysn/com/mvvm/utils/ToastUtils.java
@@ -3,9 +3,14 @@ package ysn.com.mvvm.utils;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.os.Handler;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+
+import ysn.com.mvvm.base.R;
 
 /**
  * @Author yangsanning
@@ -18,9 +23,9 @@ public class ToastUtils {
     @SuppressLint("StaticFieldLeak")
     private static Application application = null;
 
-    private static Toast mToastNormal;
     private static long ThreadID = android.os.Process.myTid();
     private static Handler handler;
+    private static Toast toast;
 
     public static void inject(@NonNull Application application) {
         ToastUtils.application = application;
@@ -56,7 +61,7 @@ public class ToastUtils {
             if (handler == null) {
                 handler = new Handler();
             }
-            handler.postDelayed(() -> makeToast(message),300);
+            handler.postDelayed(() -> makeToast(message), 300);
         }
     }
 
@@ -65,14 +70,20 @@ public class ToastUtils {
     }
 
     private static void makeToast(String msg) {
-        ToastUtils.cancel();
-        mToastNormal = Toast.makeText(application, msg, Toast.LENGTH_SHORT);
-        mToastNormal.show();
+        cancel();
+        View view = View.inflate(application, R.layout.view_toast, null);
+        TextView toastView = view.findViewById(R.id.toast_view_text);
+        toast = new Toast(application);
+        toast.setGravity(Gravity.CENTER, 1, 1);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toastView.setText(msg);
+        toast.setView(view);
+        toast.show();
     }
 
     public static void cancel() {
-        if (mToastNormal != null) {
-            mToastNormal.cancel();
+        if (toast != null) {
+            toast.cancel();
         }
     }
 }
